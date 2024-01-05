a6f5ac9a72c40bb3ea086cd4a57ec71a787da97f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-01-05 18:18:53 +0100

    chore: add TODO about matcher

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@
 // TODO: Write rejects to file
 // TODO: Feature traces and target configuration are part of the input!
 // TODO: The matcher should be exchangeable (e.g., use a trait?)
+// TODO: The matcher could use the LCS algorithm for starters; we only parse diffs to get the
+// required changes, but the matcher itself is not based on diff but on LCS; look for an LCS crate
+// on crates.io
 
 pub mod diff;
 pub mod error;
