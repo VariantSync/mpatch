18f73e41fbcedc1200dfe54ddbd986faa842600e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-01-30 21:17:39 +0100

    tests: add first integration test stubs for matcher

---
+++ tests/match.rs
@@ -0,0 +1,24 @@
+#[test]
+fn file_matches_itself() {
+    todo!();
+}
+
+#[test]
+fn one_line_added_left() {
+    todo!();
+}
+
+#[test]
+fn one_line_added_right() {
+    todo!();
+}
+
+#[test]
+fn one_line_added_each_overlap() {
+    todo!();
+}
+
+#[test]
+fn one_line_added_each_non_overlap() {
+    todo!();
+}
