5fbf2beb2eb44be0c21c3ef7c1e6fcd04f96f3bd
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-02 15:04:03 +0100

    chore: remove completed TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,8 @@
-// TODO: Align patch diff with variant diff
-// TODO: Apply changes to target file
-// TODO: Load target file from disk
-// TODO: Save target file to disk
 // TODO: Recognize file creation
 // TODO: Recognize file deletion
 // TODO: Write rejects to terminal
 // TODO: Write rejects to file
 // TODO: Feature traces and target configuration are part of the input!
-// TODO: The matcher should be exchangeable (e.g., use a trait?)
-// TODO: The matcher could use the LCS algorithm for starters; we only parse diffs to get the
-// required changes, but the matcher itself is not based on diff but on LCS; look for an LCS crate
-// on crates.io
 
 pub mod diffs;
 pub mod error;
--- tests/patch.rs
+++ tests/patch.rs
@@ -1,4 +1,3 @@
-// TODO: Where is the alignment calculated? In a matching? On a change? Someplace else?
 use mpatch::{
     patch::{AlignedPatch, Patch},
     CommitDiff, FileArtifact, LCSMatcher, Matcher,
