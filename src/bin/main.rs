4fee65513d91c98f031a8d9539ab7e96b3b3bf12
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-12 21:19:08 +0100

    cli: first broken draft for cli

--- src/bin/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // TODO: CLI argument - patch diff
-    // TODO: CLI argument - variant diff (aka. match)
-    // TODO: CLI argument - rejects file
-    println!("Hello World!");
-}
---
+++ src/bin/mpatch.rs
@@ -0,0 +1,69 @@
+use std::{env, error::Error, path::PathBuf, str::FromStr};
+
+// TODO: write rejects to file
+
+use clap::Parser;
+use mpatch::{patch::FilePatch, CommitDiff, FileArtifact, LCSMatcher, Matcher};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    let mut matcher = LCSMatcher;
+
+    let diff = CommitDiff::read(&cli.patch_file).unwrap();
+    let source_dir = PathBuf::from_str(&cli.source_dir)
+        .expect("was not able to parse path to source directory (<SOURCEDIR>)");
+    let target_dir = env::current_dir()?;
+
+    for file_diff in diff.file_diffs() {
+        let mut source_file_path = source_dir.clone();
+        // TODO: Use path stripping
+        // TODO: Test CLI
+        // TODO: Fix main
+        source_file_path.push(file_diff.source_file().path());
+
+        let mut target_file_path = target_dir.clone();
+        target_file_path.push(file_diff.target_file().path());
+
+        let source = FileArtifact::read(
+            source_file_path
+                .to_str()
+                .expect("the source directory is not a valid UTF-8 path"),
+        )
+        .unwrap();
+        let target = FileArtifact::read(
+            target_file_path
+                .to_str()
+                .expect("the target directory is not a valid UTF-8 path"),
+        )
+        .unwrap();
+
+        let matching = matcher.match_files(source, target);
+        let patch = FilePatch::from(diff.file_diffs().first().unwrap().clone());
+        let aligned_patch = patch.align_to_target(matching);
+        let actual_result = aligned_patch.apply();
+        let (actual_result, rejects) = (
+            actual_result.patched_file(),
+            actual_result.rejected_changes(),
+        );
+        if !cli.dryrun {
+            actual_result.write()?;
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long = "sourcedir")]
+    source_dir: String,
+    #[arg(long = "patchfile")]
+    patch_file: String,
+    #[arg(long = "rejectsfile")]
+    rejects_file: Option<String>,
+    #[arg(long = "strip", default_value_t = 0)]
+    strip: usize,
+    #[arg(long = "dryrun", default_value_t = false)]
+    dryrun: bool,
+}
