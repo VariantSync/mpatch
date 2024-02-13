9e97c87d65d10f0f1ed1472da26328cb12f915d4
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-13 11:24:14 +0100

    refactor(tests): extract common functionality

--- tests/patch.rs
+++ tests/patch.rs
@@ -1,7 +1,6 @@
-use mpatch::{
-    patch::{AlignedPatch, FilePatch},
-    CommitDiff, FileArtifact, LCSMatcher, Matcher,
-};
+use common::{get_aligned_patch, run_alignment_test, run_application_test};
+
+mod common;
 
 // TODO: Test multi-alignment
 // TODO: Test file creation
@@ -41,11 +40,6 @@ const NON_EXISTANT_DIFF: &str = "tests/diffs/remove_non_existant.diff";
 const EXPECTED_NON_EXISTANT_PATCH: &str = "tests/expected_patches/remove_non_existant.diff";
 const EXPECTED_NON_EXISTANT_RESULT: &str =
     "tests/samples/target_variant/version-1/remove_non_existant.c";
-
-fn read_patch(path: &str) -> FilePatch {
-    let diff = CommitDiff::read(path).unwrap();
-    FilePatch::from(diff.file_diffs().first().unwrap().clone())
-}
 
 #[test]
 fn invariant_alignment() {
@@ -92,26 +86,6 @@ fn mixed_alignment() {
     run_alignment_test(MIXED_SOURCE, MIXED_TARGET, MIXED_DIFF, EXPECTED_MIXED_PATCH);
 }
 
-fn run_alignment_test(source: &str, target: &str, diff: &str, expected_patch: &str) {
-    let source = FileArtifact::read(source).unwrap();
-    let target = FileArtifact::read(target).unwrap();
-
-    let mut matcher = LCSMatcher;
-    let matching = matcher.match_files(source, target);
-
-    let patch = read_patch(diff);
-    let expected_patch = read_patch(expected_patch);
-    let aligned_patch = patch.align_to_target(matching);
-
-    for (expected, aligned) in expected_patch
-        .changes()
-        .iter()
-        .zip(aligned_patch.changes().iter())
-    {
-        assert_eq!(expected, aligned);
-    }
-}
-
 #[test]
 fn apply_invariant() {
     let aligned_patch = get_aligned_patch(INVARIANT_SOURCE, INVARIANT_TARGET, INVARIANT_DIFF);
@@ -142,40 +116,4 @@ fn apply_non_existant() {
     let aligned_patch =
         get_aligned_patch(NON_EXISTANT_SOURCE, NON_EXISTANT_TARGET, NON_EXISTANT_DIFF);
     run_application_test(aligned_patch, EXPECTED_NON_EXISTANT_RESULT, 1);
-}
-
-fn get_aligned_patch(source: &str, target: &str, diff: &str) -> AlignedPatch {
-    let source = FileArtifact::read(source).unwrap();
-    let target = FileArtifact::read(target).unwrap();
-
-    let mut matcher = LCSMatcher;
-    let matching = matcher.match_files(source, target);
-
-    let patch = read_patch(diff);
-    patch.align_to_target(matching)
-}
-
-fn run_application_test(
-    aligned_patch: AlignedPatch,
-    expected_result: &str,
-    expected_rejects_count: usize,
-) {
-    let expected_result = FileArtifact::read(expected_result).unwrap();
-
-    let actual_result = aligned_patch.apply();
-    let (actual_result, rejects) = (
-        actual_result.patched_file(),
-        actual_result.rejected_changes(),
-    );
-
-    assert_eq!(expected_result.lines().len(), actual_result.lines().len());
-    assert_eq!(rejects.len(), expected_rejects_count);
-    for (expected, actual) in expected_result
-        .lines()
-        .iter()
-        .zip(actual_result.lines().iter())
-    {
-        println!("exp: {expected}\nact: {actual}");
-        assert_eq!(expected, actual);
-    }
 }
